1d1dcfa00f7614019b89ab810dda26ae6bc80407
Trevor Facer <trevorf@ifit.com>
2022-01-01 11:20:02 -0700

    Comment out unused code

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,23 +1,23 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import { connectToDatabase } from '../connect-to-db';
-import { bucketListModel } from '../data/models/bucket-list-item';
-import Joi from 'joi';
+// import { bucketListModel } from '../data/models/bucket-list-item';
+// import Joi from 'joi';
 
-const validate = (obj: any) => {
-  const schema = Joi.object({
-    name: Joi.string().required().alphanum(),
-    complete: Joi.bool().default(false),
-    description: Joi.string().required(),
-    imageUrls: Joi.array(),
-    activity: Joi.string().required().alphanum(),
-  })
-  const { error, value } = schema.validate(obj);
-  if (error) {
-    throw error;
-  }
-  return value;
-}
+// const validate = (obj: any) => {
+//   const schema = Joi.object({
+//     name: Joi.string().required().alphanum(),
+//     complete: Joi.bool().default(false),
+//     description: Joi.string().required(),
+//     imageUrls: Joi.array(),
+//     activity: Joi.string().required().alphanum(),
+//   })
+//   const { error, value } = schema.validate(obj);
+//   if (error) {
+//     throw error;
+//   }
+//   return value;
+// }
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
   console.log(`${JSON.stringify(event)}`);
